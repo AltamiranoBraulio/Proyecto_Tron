db93c1534f1a11462ea8ae87b467f10c86303189
JOAQUIN MONROY NAVARRO <143145259+MONROYJ04@users.noreply.github.com>
2023-12-06 02:42:35 -0600

    clase Player

---
+++ include/Player.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef TRONGAME_PLAYER_H
+#define TRONGAME_PLAYER_H
+
+class Player
+{
+
+public:
+    int xPosition;
+    int yPosition;
+    int score;
+    int covered;
+
+    Player() = default;;
+
+    void changePosition(int x, int y);
+};
+
+#endif // TRONGAME_PLAYER_H
