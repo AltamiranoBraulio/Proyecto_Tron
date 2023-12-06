6bdd9e0f4c37353d93e16562c046bab53d9c2b73
JOAQUIN MONROY NAVARRO <143145259+MONROYJ04@users.noreply.github.com>
2023-12-06 02:40:36 -0600

    clase Menu

---
+++ include/Menu.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef TRONGAME_MENU_H
+#define TRONGAME_MENU_H
+
+#define MAX_NUMBER_OF_ITEMS 2
+
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+
+class Menu
+{
+private:
+    int selectedItemIndex;
+    Font font;
+    Text menu[MAX_NUMBER_OF_ITEMS];
+    Text instruction;
+
+public:
+    float width;
+    float height;
+
+    Menu(float width, float height);
+
+    void draw(RenderWindow &window);
+
+    void moveLeft();
+
+    void moveRight();
+
+    int getPressedItem() const { return selectedItemIndex; }
+
+    int startMenu() const;
+
+    void setProperties();
+};
+
+#endif // TRONGAME_MENU_H
