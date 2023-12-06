fa6294080b8138a5a09a214fc7f4b47db1a651c6
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2023-12-06 02:22:42 -0600

    clase game bar

---
+++ include/GameBar.hpp
@@ -0,0 +1,29 @@
+
+#ifndef TRONGAME_GAMEBAR_H
+#define TRONGAME_GAMEBAR_H
+
+#include "SFML/Graphics.hpp"
+#include "Player.hpp"
+
+using namespace sf;
+
+class GameBar {
+
+public:
+    Font font;
+    Text p1Score;
+    Text p2Score;
+    Text p1Covered;
+    Text p2Covered;
+    Text timer;
+
+    GameBar();
+
+    static Font loadFont();
+
+    void updateTexts(Player &player1, Player &player2, float time);
+
+    void setTextProperties();
+};
+
+#endif //TRONGAME_GAMEBAR_H
