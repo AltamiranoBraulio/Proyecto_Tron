cfaf18bd3542094a288723f0595e8f639a9beae9
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2023-12-06 22:01:44 -0600

    acomodo de clases ctrl+a+k+f(main)

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
 #include "GameEngine.hpp"
 #include "Menu.hpp"
 
-int main() {
+int main()
+{
     std::cout << "Welcome in Tron Game !!!" << std::endl;
-    //set board parameters
+    // set board parameters
     Board board = Board(60, 34, 20);
-    Menu menu((float) (board.size * board.W), (float) (board.size * board.H));
-    //run menu window
+    Menu menu((float)(board.size * board.W), (float)(board.size * board.H));
+    // run menu window
     int mode = menu.startMenu();
-    if (mode == -1 ) {printf("Menu closed!") ;return 0;}
-    //init players and start game with selected mode
+    if (mode == -1)
+    {
+        printf("Menu closed!");
+        return 0;
+    }
+    // init players and start game with selected mode
     Player player1 = Player();
     Player player2 = Player();
     GameEngine game = GameEngine(board, player1, player2, 40.f);
